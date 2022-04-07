fa5b1d114ef1348331caacf87033cc62998bc503
root <root@pop-os.localdomain>
2022-04-08 00:28:18 +0300

    Init

---
+++ ac_off.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 2 > /proc/sys/vm/laptop_mode
+echo min_power > /sys/class/scsi_host/host0/link_power_management_policy
+echo min_power > /sys/class/scsi_host/host1/link_power_management_policy
+echo powersave > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
+echo powersave > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+echo 130 > /sys/class/backlight/amdgpu_bl0/brightness
+echo 130 > /sys/class/backlight/amdgpu_bl0/actual_brightness
